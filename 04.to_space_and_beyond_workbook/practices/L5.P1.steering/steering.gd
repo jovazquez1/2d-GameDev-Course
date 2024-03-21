1f28005ab15128f171be123141b5a68c893d592a
Josh <jovazquez1@s.sfusd.edu>
2024-03-21 11:45:16 -0700

    L6

--- 04.to_space_and_beyond_workbook/practices/L5.P1.steering/steering.gd
+++ 04.to_space_and_beyond_workbook/practices/L5.P1.steering/steering.gd
@@ -9,9 +9,9 @@ func _process(delta: float) -> void:
 	direction.y = Input.get_axis("move_up", "move_down")
 	if direction.length() > 1.0:
 		direction = direction.normalized()
-	var desired_velocity := max_speed * direction #
-	var steering := desired_velocity - velocity #
-	velocity += steering * steering_factor * delta #
+	var desired_velocity := max_speed * direction 
+	var steering := desired_velocity - velocity 
+	velocity += steering * steering_factor * delta 
 	position += velocity * delta
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
