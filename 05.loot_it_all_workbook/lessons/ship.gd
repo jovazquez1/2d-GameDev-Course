71617c6f8c4661b58035c0c2b50d2e86cfeff4e5
Josh <jovazquez1@s.sfusd.edu>
2024-04-04 13:06:59 -0700

    L6 (practices don't work)

--- 05.loot_it_all_workbook/lessons/ship.gd
+++ 05.loot_it_all_workbook/lessons/ship.gd
@@ -3,6 +3,7 @@ var max_speed := 1200.0
 var velocity := Vector2(0, 0)
 var steering_factor := 3.0
 var health := 10
+var gem_count := 0
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
@@ -25,4 +26,11 @@ func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 
 func _on_area_entered(area_that_entered: Area2D) -> void:
-	set_health(health + 10)
+	if area_that_entered.is_in_group("gem"):
+		set_gem_count(gem_count + 1)
+	elif area_that_entered.is_in_group("healing_item"):
+		set_health(health + 10)
+
+func set_gem_count(new_gem_count: int) -> void:
+	gem_count = new_gem_count
+	get_node("UI/GemCount").text = "x" + str(gem_count)
